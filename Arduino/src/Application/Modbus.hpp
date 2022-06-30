1103e9fabf270675981319bd6787ea73df4c5135
Jovica Jovetic <jovicanjovetic@gmail.com>
2022-06-30 14:28:26 +0200

    Modbus registar i konstruktor

--- Arduino/src/Application/Modbus.hpp
+++ Arduino/src/Application/Modbus.hpp
@@ -1,7 +1,28 @@
 #include <stdint.h>
 
-class Modbus{
+struct ModbusRegister{
+    uint8_t Address;
+    uint8_t FunctionCode;
+    uint32_t dword;
+
+    union Data
+    {   uint8_t byte0;
+        uint8_t byte1;
+        uint8_t byte2;
+        uint8_t byte3;
+        
+    };
     
+    
+
+};
+class Modbus{
+    public:
+        Modbus(){
+            
+
+        }
+
 
 }
 
